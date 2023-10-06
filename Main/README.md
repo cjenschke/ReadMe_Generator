f718522c4f7500b5553dc4c05d827c946cf1e845
Cimarron <cimarron13@mac.com>
2023-10-06 16:13:58 -0500

    breaktime

--- Main/README.md
+++ Main/README.md
@@ -13,7 +13,7 @@
   
 
   ## License 
- ![GitHub license](https://img.shields.io/badge/license-GNU GPL v3-blue.svg)(- [License](#license))
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](#license)(https://opensource.org/licenses/MIT)
 
   ## Features
   
--- Main/utilities/generateMarkdown.js
+++ Main/utilities/generateMarkdown.js
@@ -2,15 +2,22 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license || license === "None") return "";
-  return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  return `[![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)](#license)`;
   
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license || license === "None") return "";
-  return `- [License](#license)`;
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "Apache 2.0") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "GNU GPLv3") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -19,7 +26,7 @@ function renderLicenseSection(license) {
   if (license){
     const licenseBadge = renderLicenseBadge(license);
     const licenseLink = renderLicenseLink(license);
-    return `## License \n\ ${licenseBadge}(${licenseLink})`;
+    return `## License \n${licenseBadge}(${licenseLink})`;
   } else {
     return '';
   }
