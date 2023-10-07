76f942c39cf5132f712fd6d8f53cd302cdc64520
Cimarron <cimarron13@mac.com>
2023-10-06 20:18:02 -0500

    Deleted TO DO comments

--- Main/index.js
+++ Main/index.js
@@ -1,9 +1,8 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utilities/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+
 const questions = [
   {
     type: "input",
@@ -68,7 +67,7 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     if (err) throw err;
@@ -76,7 +75,7 @@ function writeToFile(fileName, data) {
   });
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
   inquirer.prompt(questions).then((answers) => {
     const markdownContent = generateMarkdown(answers);
@@ -84,5 +83,5 @@ function init() {
   });
 }
 
-// Function call to initialize app
+
 init();
